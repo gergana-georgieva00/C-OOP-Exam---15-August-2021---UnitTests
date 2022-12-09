dab4873672d7e6593b3a7cd78a701a5762824990
Gergana Georgieva <gerganagergana1@gmail.com>
2022-12-09 18:15:29 +0200

    Add project files.

---
+++ Robots.Tests/RobotsTests.cs
@@ -0,0 +1,24 @@
+﻿namespace Robots.Tests
+{
+    using NUnit.Framework;
+    using System;
+
+    public class RobotsTests
+    {
+        private RobotManager robotManager;
+        private Robot robot;
+
+        [SetUp]
+        public void SetUp()
+        {
+            robotManager = new RobotManager(5);
+            robot = new Robot("robotName", 100);
+        }
+
+        [Test]
+        public void CapacityCannotBeLessThanZero()
+        {
+            Assert.Throws<ArgumentException>(() => robotManager = new RobotManager(-2));
+        }
+    }
+}
